8e03b0d0a6cca09ae05844986ad23a53c0192691
Thainá Israel <thainaluizaisrael@gmail.com>
2025-06-07 19:12:53 -0300

    adicionando expected output

--- tarefas.py
+++ tarefas.py
@@ -2,7 +2,17 @@ from crewai import Task
 from agentes import financas, coach, analista
 
 tasks = [
-    Task(description="Criar plano mensal para alcançar metas financeiras", agent=financas),
-    Task(description="Gerar mensagem motivacional com base no progresso.", agent=coach),
-    Task(description="Avaliar se a meta será atingida no prazo.", agent=analista),
+
+    
+    Task(description="Criar plano mensal para alcançar metas financeiras", 
+         expected_output="Plano mensal criado com sucesso.",
+         agent=financas,),
+    
+    Task(description="Gerar mensagem motivacional com base no progresso.",
+        expected_output="Mensagem motivacional gerada com sucesso.",
+        agent=coach),
+    
+    Task(description="Avaliar se a meta será atingida no prazo.",
+          expected_output="Análise de progresso concluída.",
+          agent=analista),
 ]
